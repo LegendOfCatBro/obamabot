816d1a8e40ca840804574b7283c093d90e361229
BuildTools <unconfigured@null.spigotmc.org>
2020-09-10 09:30:35 -0700

    fixed starboard

--- cogs/database.py
+++ cogs/database.py
@@ -20,7 +20,7 @@ class database(commands.Cog):
             except:
                 print('Exception on set: Unable to find desired channel!')
                 raise commands.BadArgument
-            args = (str(ctx.guild.id), str(ctx.guild.name), channel, str(ctx.guild.name), str(ch.id))
+            args = (str(ctx.guild.id), str(ctx.guild.name), str(ch.id), str(ctx.guild.name), str(ch.id))
             conn = sqlite3.connect('bot.db')
             c = conn.cursor()
             c.execute("SELECT * FROM guilds")
@@ -28,24 +28,42 @@ class database(commands.Cog):
                 c.execute("INSERT INTO guilds (gid, gname, starid) VALUES (?,?,?) ON CONFLICT(gid) DO UPDATE SET gname=?, starid=?",args)
             if column == "roleid":
                 c.execute("INSERT INTO guilds (gid, gname, roleid) VALUES (?,?,?) ON CONFLICT(gid) DO UPDATE SET gname=?, roleid=?",args)
-            else:
-                print('Exception on set: Unable to find desired function')
-                raise commands.BadArgument
+
             embed = discord.Embed(
                 title=f'Successfully bound {channel} to {column}', 
                 colour=ctx.author.color
                 )
             print(f'{channel} successfully bound to {column} in {ctx.guild.name}')
+            conn.commit()
+            conn.close
             await ctx.send(embed=embed)    
-
+        @bot.command(
+            name='readb',
+            description='Reads out all entries in the database row for this guild'
+        )
+        async def readb(ctx):
+            conn = sqlite3.connect('bot.db')
+            conn.row_factory = sqlite3.Row
+            c = conn.cursor()
+            c.execute('SELECT * FROM guilds WHERE gid=?',(str(ctx.guild.id),))
+            r = c.fetchone()
+            t = r.keys()
+            print(r)
+            e = discord.Embed(
+                title=f'Database data for {ctx.guild.name}', 
+                colour=ctx.author.color
+                )
+            g = 0
+            for item in r: 
+                e.add_field(name=f'{t[g]}', value=f'{item}', inline=False)
+                g += 1
+            await ctx.send(embed=e)
 
 
 def setup(bot):
     bot.add_cog(database(bot))
     conn = sqlite3.connect('bot.db')
     c = conn.cursor()
-       
-    starboard_name = 'starboard'
     for guild in bot.guilds:
         gnt = str(guild.name)
         git = str(guild.id)
--- cogs/starboard.py
+++ cogs/starboard.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import sqlite3
 from discord.ext import commands
 from discord.utils import get
 from discord.utils import find
@@ -17,10 +18,13 @@ class starboard(commands.Cog):
         starboard_threshold = 1
         starboard_color = 0xF9A602
     
-    
+        conn = sqlite3.connect('bot.db')
+        c = conn.cursor()
+        c.execute('SELECT starid FROM guilds WHERE gid=?',(str(payload.guild_id),))
+        bid = c.fetchone()
+        board = self.bot.get_channel(int(bid[0]))
         guild = self.bot.get_guild(payload.guild_id)
-        board = find(lambda m: m.name == starboard_name, guild.text_channels)
-        sauce = self.bot.get_channel(payload.channel_id) 
+        sauce = guild.get_channel(payload.channel_id)
         msg = await sauce.fetch_message(payload.message_id)
         goodtypes = ('.png', '.PNG', '.jpg', '.jpeg', '.JPG', '.JPEG', '.gif', '.gifv')
         star = get(msg.reactions, emoji=starboard_emoji)
@@ -45,8 +49,10 @@ class starboard(commands.Cog):
             try:
                 await board.send(embed = e)   
             except:
-                x = discord.Embed(title='Error!', description='Starboard channel not found!', color =0x7C0A02) 
+                x = discord.Embed(title='Error!', description='Error sending embed to starboard! The starboard channel ID could be invalid.', color =0x7C0A02) 
                 await sauce.send(embed = x)
+                await sauce.send(embed = e)
+           
             
 def setup(bot):
     bot.add_cog(starboard(bot))
