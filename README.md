96c94776822fa8d9f071c65929a0ac11925ceb13
Eric Palmer <torracat33@gmail.com>
2020-10-18 17:51:53 -0700

    Added dice rolling cog, updated readme

--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 
 while setting up this repo i almost accidentally deleted everything so :)
 
-it can do lotsa cool things such as:
+obama is a discord bot i decided to make, he can do lotsa cool things such as:
 
 -manage a starboard
 
 -react to your messages with images or text
 
--give roles when a message is reacted on
+-manage self-assignable roles (kind of? i sorta half-assed it)
+
+-produce random numbers from a dice formula, supporting modifiers and multiple types of dice at once\! 
 
 -make me feel good about myself
 
-this is an ongoing personal project of mine, im an amatuer for sure
+this is an ongoing personal project of mine, im an amatuer for sure. i will add more features when i feel like it.  there is basically no documentation or comments because i am a lazy piece of shit. most of the skills used in this project were gained from google, looking at docs, trial and error, and asking people for help.
 
-i learned to make this bot through trial and error, ~~copying~~ looking at other people's code, mindlessly staring at the docs for several hours, and asking others for help
+my discord is Satan.png \#9600 hmu if you have any questions, suggestions, or requests. i'm open for any ideas you wanna throw at me and you can totally have obama in your server :)
+
+pls dont yoink without crediting me cuz theres like copyright and stuff and also thats just a dick move
+
---
+++ cogs/rng.py
@@ -0,0 +1,83 @@
+import os
+import discord
+import random
+from discord.ext import commands
+
+class rng(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot   
+    @commands.command(name='roll', description='roll some virtual dice. Supports multiple types of dice and modifiers!')
+    async def roll2(self, ctx, *, args):
+        embed = discord.Embed(title='Dice Roll', color=ctx.author.color)
+        arg = args.split(' ')
+        dices = []
+        pmod = []
+        nmod = []
+        errors = 0
+        for x in arg:
+            f = x.split('d')
+            g = x[1:]
+            if "d" in x and len(x) > 2 and len(f) == 2 and IsInt(f[0]) == True and IsInt(f[1]) == True and int(f[0]) > 0 and int(f[1]) > 0:
+                dices.append(x)
+            elif x.startswith('+') == True and len(x) > 1 and IsInt(g) == True:
+                pmod.append(int(x[1:]))
+            elif x.startswith('-') == True and len(x) > 1 and IsInt(g) == True:
+                nmod.append(int(x[1:]))
+            elif errors == 24:
+                embed.add_field(name='AAAAAAAAAAA', value='TOO MANY ERRORS JESUS FUCKING CHRIST JUST TRY AGAIN')
+                await ctx.send(embed=embed)
+                return
+            else:
+                embed.add_field(name='Error!', value=f'Invalid argument {x}, ignoring!')
+                errors +=1
+        bkdns = []
+        sum = 0
+        
+        if dices:
+            for y in dices:
+                random.seed()
+                dice = y.split('d')
+                a = int(dice[0])
+                b = int(dice[1])
+                s = 0
+                bstr = f'({a}d{b}): '
+                nums = []
+                for x in range(a):
+                    t = random.randint(1,b)
+                    nums.append(t)
+                    bstr += f'{t}, '
+                for x in nums:
+                    s = x + s
+                bstr = bstr[:-2]
+                bstr += f' [{s}]'
+                sum = sum + s
+                bkdns.append(bstr)
+            bd = 'Breakdown: {'
+            for x in bkdns:
+                bd += f'{x} '
+            bd = bd[:-1]
+            bd += '}'
+            bd += f'={sum}'
+        else: 
+            embed.add_field(name='Error!', value='No valid rolls given! Remember to use the format XdY, with X being the number of dice and Y being the number of sides on each die.')
+            await ctx.send(embed=embed)
+            return
+        if pmod or nmod:
+            bd += f'; {sum} '
+            for x in pmod:
+                bd += f'+{x} '
+                sum = sum + x
+            for x in nmod:
+                bd += f'-{x} '
+                sum = sum - x
+            bd += f'= {sum}'   
+        embed.add_field(name=f'You rolled {sum}!', value=bd)
+        await ctx.send(embed=embed)
+def setup(bot):
+    bot.add_cog(rng(bot))
+def IsInt(x):
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
